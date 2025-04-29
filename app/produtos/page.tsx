abab78b3dc5831040f9e8e09bae45f51be59eae7
Adeilton <Adeilton@DESUL2-DTF7D8.educacao.intragov>
2025-04-29 11:43:30 -0300

    produtos atualização

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,12 +1,13 @@
 import ListaProdutos from "../ListaProdutos";
-import { products } from "../dados";
 
-export default function ProdutosPage(){
-	
+export default async function ProdutosPage(){
+	const cn = await fetch("https://acpteste.netlify.app/produtos");
+	const prod = await cn.json();
+
 	return (
 	<div className="container mx-auto p-8">		
 			<h1 className="text-4xl font-bold mb-8">Produtos</h1>		
-			<ListaProdutos produtos={products} />		
+			<ListaProdutos produtos={prod} />		
 	</div>
 	)
 	
