47ecec439dc4d27adf22841e2f6cb301f19e87b6
Adeilton <Adeilton@DESUL2-DTF7D8.educacao.intragov>
2025-05-06 14:52:18 -0300

    função add carrinho

--- app/carrinho/page.tsx
+++ app/carrinho/page.tsx
@@ -2,7 +2,9 @@ import CartProd from "./carProd";
 
 export default async function CartPage(){
 	
-	const cn = await fetch("https://acpteste.netlify.app/api/users/1/cart");
+	const cn = await fetch("https://acpteste.netlify.app/api/users/1/cart", {
+		cache: "no-cache"
+	});
 	const carprodutos = await cn.json() 
 
 	return(
