cd764db9aca1fbf9074bfcaf1fc38a973f95eccd
Adeilton <Adeilton@DESUL2-DTF7D8.educacao.intragov>
2025-05-06 14:49:36 -0300

    função add carrinho

--- app/ListaProdutos.tsx
+++ app/ListaProdutos.tsx
@@ -1,10 +1,29 @@
+'use client';
+
 import  Image  from 'next/image';
 import  Link  from 'next/link';
 import { Product } from "./dados";
+import { useState } from 'react';
 
 
-export default function ListaProdutos({produtos}:{produtos: Product[]}){
+export default function ListaProdutos({produtos, prodInicial}:{produtos: Product[], prodInicial: Product[]}){
 
+	const [carProd, setCar] = useState(prodInicial); 
+
+	async function addToCart(productId:string){
+		const cn = await fetch("https://acpteste.netlify.app/api/users/1/cart",{
+			method: "POST", 
+			body: JSON.stringify({
+				productId
+			}),
+			headers:{
+				'Content-Type' : 'application/json'
+			}
+		});
+
+		const atualizaCar = await cn.json();
+		setCar(atualizaCar)
+	}
 	return (
 		<div>
 			{
@@ -13,6 +32,7 @@ export default function ListaProdutos({produtos}:{produtos: Product[]}){
 						<Image src={"/"+p.imageUrl} alt="Imagem do produto" width={150} height={150}  />
 						<h2>{p.name}</h2>
 						<p>R${p.price}</p>
+						<button onClick={() => addToCart(p.id)}>Comprar</button>
 
 					</Link>
 				))
--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -4,10 +4,13 @@ export default async function ProdutosPage(){
 	const cn = await fetch("https://acpteste.netlify.app/api/produtos");
 	const prod = await cn.json();
 
+	const cn2 = await fetch("https://acpteste.netlify.app/api/users/1/cart");
+	const cartProd = await cn2.json();
+
 	return (
 	<div className="container mx-auto p-8">		
 			<h1 className="text-4xl font-bold mb-8">Produtos</h1>		
-			<ListaProdutos produtos={prod} />		
+			<ListaProdutos produtos={prod} prodInicial={cartProd} />		
 	</div>
 	)
 	
